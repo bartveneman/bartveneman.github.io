6d7c7a469e2b84e76250b8544b59f9b693ea40d5
Bart Veneman <bartveneman@gmail.com>
2014-05-20 21:49:27 +0200

    Initiate v4

---
+++ README.md
@@ -0,0 +1,30 @@
+# www.bveneman.nl
+The goal of this site is to offer an insight to a developers daily struggles, wins and ideas. 
+A secondary goal is making this site as fast as possible. This means:
+
+- No webfonts;
+- No external stylesheet(s);
+- Only Javascript for Google Analytics;
+- Hardly any images, and preferrably SVG's;
+- Serving from a subdomain, because it's supposed to be faster;
+
+Do you spot any potential performance wins?
+
+## Gulp tasks
+Before running tasks, make sure that all devDependencies are installed via npm.
+
+### `gulp sass`
+1. Compile SASS into CSS
+2. Prefix all the things
+3. Report filesize
+4. Combine media queries
+5. Minify CSS
+6. Report optimized filesize
+7. Report gzipped filesize
+8. Save as...
+
+### `gulp html`
+Minify HTML inside the `_site` folder, but keep the comments.
+
+### `gulp watch`
+Watch files for changes to run the above listed tasks.
---
+++ gulpfile.js
@@ -0,0 +1,69 @@
+var gulp = require("gulp"),
+	sass = require("gulp-ruby-sass"),
+	autoprefixer = require("gulp-autoprefixer"),
+	minify = require("gulp-minify-css"),
+	size = require("gulp-size"),
+	cmq = require('gulp-combine-media-queries'),
+	minifyhtml = require("gulp-minify-html"),
+	lr = require("tiny-lr"),
+	server = lr();
+
+/**
+ * 1. Compile SASS into CSS
+ * 2. Prefix all the things
+ * 3. Report filesize
+ * 4. Combine media queries
+ * 5. Minify CSS
+ * 6. Report optimized filesize
+ * 7. Report gzipped filesize
+ * 8. Save as...
+ */
+gulp.task("sass", function () {
+	gulp.src("_assets/scss/master.scss")
+		.pipe(sass({ // 1
+			style: "compressed",
+			noCache: true,
+			precision: 2
+		}))
+		.pipe(autoprefixer('last 2 versions', 'ie 9', 'android 4', 'android 2', 'ios 6')) //2
+		.pipe(size({ // 3
+			title: "Uncompressed"
+		}))
+		.pipe(cmq({ // 4
+			log: true
+		}))
+		.pipe(minify({ // 5
+			keepSpecialComments: 0,
+            keepBreaks: false
+		}))
+		.pipe(size({ // 6
+			title: "Compressed"
+		}))
+		.pipe(size({ // 7
+			title: "gzipped",
+			gzip: true
+		}))
+		.pipe(gulp.dest("_includes")); //8
+});
+
+gulp.task("html", function () {
+	gulp.src("_site/**/*.html")
+		.pipe(minifyhtml({
+			quotes: true
+		}))
+		.pipe(gulp.dest("_site"));
+});
+
+/**
+ * Watch task: to run tasks after files have changed
+ */
+gulp.task("watch", ["sass", "html"], function () {
+	server.listen(35729, function (err) {
+        if (err) {
+            return console.log(err);
+        }
+
+        gulp.watch("_assets/scss/**/*.scss", ["sass"]);
+        gulp.watch("_site/**/*.html", ["html"]);
+    });
+});
