bcf72b671ee310b59d03933454b0cf1f4ea60ba3
Bart Veneman <1536852+bartveneman@users.noreply.github.com>
2020-01-04 12:23:10 +0100

    inline and minify all css (#5)

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,16 @@
+const cleanCss = require('clean-css')
+
 module.exports = eleventyConfig => {
 	eleventyConfig.setTemplateFormats(['md', 'html'])
+
 	eleventyConfig.addPassthroughCopy('img')
 	eleventyConfig.addPassthroughCopy('*.png')
 	eleventyConfig.addPassthroughCopy('favicon.ico')
+
 	eleventyConfig.addLayoutAlias('default', 'layouts/default.html')
 	eleventyConfig.addLayoutAlias('post', 'layouts/post.html')
+
+	eleventyConfig.addFilter('cssmin', function(code) {
+		return new cleanCss({}).minify(code).styles
+	})
 }
